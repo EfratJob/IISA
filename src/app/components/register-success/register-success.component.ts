d6af5ca0b7540fb051e8514e99a706e015ac0232
efart-tichnut <89773704+efart-tichnut@users.noreply.github.com>
2025-09-09 00:39:09 +0300

    register success style

--- src/app/components/register-success/register-success.component.ts
+++ src/app/components/register-success/register-success.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { RouterModule } from '@angular/router';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-register-success',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './register-success.component.html',
   styleUrl: './register-success.component.css',
   animations: [
